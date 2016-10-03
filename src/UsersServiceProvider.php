7ff12987794188bb5a726d1ceb397dfdb3c0f927
Vitor Barroso <vitor.vgb@gmail.com>
2016-10-03 12:59:19 +0000

    service provider

--- src/UsersServiceProvider.php
+++ src/UsersServiceProvider.php
@@ -13,7 +13,36 @@ class UsersServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // routes
+
         include __DIR__.'/routes.php';
+
+        // migrations
+
+        $this->publishes([
+            __DIR__.'/migrations/' => database_path('/migrations')
+        ], 'migrations');
+
+        // assets
+
+        $this->publishes([
+            __DIR__.'/resources/assets' => public_path('vendor/users'),
+        ], 'assets');
+
+        // views
+
+        $this->loadViewsFrom(__DIR__.'/views', 'users');
+
+        $this->publishes([
+            __DIR__.'/resources/views' => base_path('resources/views/vendor/users'),
+        ], 'views');
+
+        if(config('app.debug')) {
+            \Artisan::call('vendor:publish', [
+                '--tag' => 'views',
+                '--force' => true
+            ]);
+        }
     }
 
     /**
